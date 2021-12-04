d0e66103d0e15e06b48e7a7d1c0b4db651ba2a21
Augusto P <aug.porto@hotmail.com>
2021-12-03 23:40:28 -0400

    Exercise 8: Retrieving a map, to the MapView in fragment_tracking, as a GoogleMap object and handling the map's lifecycle in TrackingFragment

--- app/src/main/java/com/example/runningappdaggercourse/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningappdaggercourse/ui/fragments/TrackingFragment.kt
@@ -1,9 +1,16 @@
 package com.example.runningappdaggercourse.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningappdaggercourse.R
+import com.example.runningappdaggercourse.databinding.FragmentTrackingBinding
 import com.example.runningappdaggercourse.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,4 +20,72 @@ class TrackingFragment: Fragment(R.layout.fragment_tracking) {
      * build.gradle file). As Dagger manages all ViewModels behind the scenes, it will select the correct one and assign it here.
      */
     private val viewModel: MainViewModel by viewModels()
+
+    lateinit var binding: FragmentTrackingBinding
+
+    /**To use Google Maps in an app, in the XML file we can either use a MapView or a MapFragment, which is just a fragment that contains
+     * a MapView. The MapFragment component is used to take advantage of the fragment's lifecycle functions to handle the map's lifecycle.
+     * However, when we already have a fragment, it's best to use MapView, as using fragments inside fragments is not good for the
+     * performance. Then, when using MapView, we need to handle the MapView's lifecycle by ourselves, by calling its lifecycle functions
+     * inside the corresponding fragment's lifecycle functions (e.g. onStart, onResume, onPause etc.).
+     * To get a map, we can use the MapView's getMapAsync method and we can set the callback function to be executed when it retrieves
+     * the map, which can be saving it in a variable of type GoogleMap.
+     */
+    private var map: GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_tracking,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    //onLowMemory is called when the device is running low on memory, in order to save some resources.
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    //Helps caching the map that was loaded, so we don't need to load it again when we come back to the app.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
 }
