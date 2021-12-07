12089ecd4dc997613990657fccd344a1d918b740
Augusto P <aug.porto@hotmail.com>
2021-12-06 22:16:44 -0400

    Exercise 11: Using FusedLocationProviderClient to request updates of the location and adding them to the pathPoints LiveData in the callback

--- app/src/main/java/com/example/runningappdaggercourse/services/TrackingService.kt
+++ app/src/main/java/com/example/runningappdaggercourse/services/TrackingService.kt
@@ -1,5 +1,6 @@
 package com.example.runningappdaggercourse.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -7,20 +8,37 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.runningappdaggercourse.R
 import com.example.runningappdaggercourse.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningappdaggercourse.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.runningappdaggercourse.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningappdaggercourse.other.Constants.ACTION_STOP_SERVICE
+import com.example.runningappdaggercourse.other.Constants.FASTEST_LOCATION_UPDATE_INTERVAL
+import com.example.runningappdaggercourse.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.runningappdaggercourse.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.runningappdaggercourse.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.runningappdaggercourse.other.Constants.NOTIFICATION_ID
+import com.example.runningappdaggercourse.other.TrackingUtility
 import com.example.runningappdaggercourse.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
+
+//We can create custom types using typealias, which substitute complex types.
+typealias Poliline = MutableList<LatLng>        //A Poliline can be seen as a continuous path in our app, composed of many individual LatLng points interconnected with lines.
+typealias Polilines = MutableList<Poliline>     //Polilines would be the group of paths where the app was tracking the run.
 
 /**Services are a component that can perform long-running operation (such as handle network transactions, play music, edit files, interact with
  * content providers etc.), even when the user is not interacting with the app, or that can supply functionality for other apps to use.
@@ -46,6 +64,32 @@ import timber.log.Timber
 class TrackingService: LifecycleService() {
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+
+//        val pathPoints = MutableLiveData<MutableList<MutableList<LatLng>>>()        //LatLng is an object that indicates the coordinates of a point in the map. To indicate a continuous path, we could use a list of LatLng objects, but as the user might want to pause the tracking, walk a little and later start tracking the run again, we need to consider these paths separately, needing a list of the paths (i.e. a list of lists of LatLng objects).
+        val pathPoints = MutableLiveData<Polilines>()           //This line is the same as the above, but using a typealias created by us at the start of the file.
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this) {
+            updateLocationTracking(it)
+        }
+    }
+
     /**The onStartCommand is called everytime the service receives a command (an Intent with an action attached to it, that was sent through
      * the startService method). Here, we check if the received intent is not null and then check the action to be executed.
      */
@@ -71,12 +115,81 @@ class TrackingService: LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    /**This function adds a new Poliline, i.e. starts a new route that is being tracked. If the list of routes being tracked is not null
+     * (there is already a route that was tracked), we add a new empty list that will represent the new route to track. If the list of
+     * routes is null, we also create the list that will contain lists that represent the routes.
+     */
+    private fun addEmptyPoliline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+    /**This function takes in a Location and adds it as a LatLng to the last Poliline.*/
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    /**We can use a Fused Location Provider to get the location at a consistent basis (periodically after a set interval or after
+     * certain distance). We just need to provide a LocationRequest and a LocationCallback to the requestLocationUpdates method.
+     * The LocationRequest contains the interval, which is the time in milliseconds around which the locations will be provided (can
+     * be less or more), the fastestInterval, which limits how frequent the locations will be received (to avoid it receiving too many
+     * locations and wasting power), and the priority, which determines if the priority is accuracy or power saving. The callback needs
+     * to be an object of a class that implements LocationCallback, which will receive the resulting location and process it in its
+     * onLocationResult method. We also need to provide a Looper, which is message loop in a thread.
+     */
+    @SuppressLint("MissingPermission")      //The method fusedLocationProviderClient.requestLocationUpdates requires a checking the location permission. As we are checking the permissions with EasyPermissions, in the hasLocationPermissions method, it doesn't recognize the permission check and it would indicate an error, which is why we need to suppress it.
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if(isTracking) {
+            if(TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest.create().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_UPDATE_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    /**We can create a callback object to receive the location, which can be done with an anonymous class of LocationCallback, through
+     * the onLocationResult method. The result contains a list of Locations, which can be added to the Poliline.
+     */
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            if(isTracking.value!!) {
+                result?.locations?.let {locations ->
+                    for(location in locations) {
+                        addPathPoint(location)
+                        Timber.d("Latitude: ${location.latitude}, Longitude: ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
     /**To make a foreground service, we need to configure it as so and also display the notification. For Android Oreo and later, we also
      * need to create a notification channel to display a notification (notification channels are the categories of notifications, which
      * allow the user to choose to display or to hide). To create a Notification object, we need to use the Builder pattern, with
      * NotificationCompat.Builder. Then, we pass the notification id and the Notification object to the startForeground function.
      */
     private fun startForegroundService() {
+
+        addEmptyPoliline()      //When we start the service, we are also starting to track the location, so we have to add a Poliline to contain the route that will be tracked.
+        isTracking.postValue(true)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager     //NotificationManager is a Android framework service that allows us to show our notifications.
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
